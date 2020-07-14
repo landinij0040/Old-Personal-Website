ab9d2e8370319dd9e6b0ac354da62515c8fae059
Isaiah Landin <ijlandin@miners.utep.edu>
2020-07-13 21:24:37 -0600

    Added Resume

---
+++ app.js
@@ -0,0 +1,13 @@
+//jshint esversion:6
+const express = require("express");
+const app = express();
+
+app.set('view engine', 'ejs');
+
+app.get("/", function(req,res){
+	res.render("home");
+});
+
+app.listen(process.env.PORT || 3000, function(){
+	console.log("Server started on port 3000");
+});
